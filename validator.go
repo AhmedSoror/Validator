eb2bdc164968550578ad1a9c2216aaca55d31b9b
AhmedSoror <ahmedsoror438@gmail.com>
2023-05-14 22:04:07 +0200

    add comments to the code

--- validator.go
+++ validator.go
@@ -6,31 +6,47 @@ import (
 	"io/ioutil"
 )
 
+// Define the structure for the AST representation
+
+// Program represents the top-level structure of the program.
 type Program struct {
-	Functions []Function `json:"functions"`
+	// A program contains one or more function declarations.
+	Functions []Function `json:"functions"` // List of function declarations
 }
 
+// Function represents a function declaration.
 type Function struct {
-	Name      string   `json:"name"`
-	Arguments []string `json:"arguments"`
-	Body      Block    `json:"body"`
-	Calls     []string `json:"calls"`
-	Declared  []string `json:"declared"`
+	// A function declaration contains a block specifying the function body.
+	/*
+		Extra added info:
+			- function identifier "name"
+			- function arguments
+			- function calls
+			- declared variables in the function
+	*/
+	Name      string   `json:"name"`      // Name of the function
+	Arguments []string `json:"arguments"` // List of function arguments
+	Body      Block    `json:"body"`      // Function body
+	Calls     []string `json:"calls"`     // List of function calls
+	Declared  []string `json:"declared"`  // List of declared variables
 }
 
+// Block represents a block of statements.
 type Block struct {
-	Statements []Statement `json:"statements"`
+	// A block contains zero or more statements.
+	Statements []Statement `json:"statements"` // List of statements in the block
 }
 
+// Statement represents an individual statement.
 type Statement struct {
-	Type          string   `json:"type"`
-	Variables     []string `json:"variables,omitempty"`
-	Uses          []string `json:"uses,omitempty"`
-	Block         Block    `json:"block,omitempty"`
-	Calls         []string `json:"calls,omitempty"`
-	Parameters    []string `json:"arguments,omitempty"`
-	OperationType string   `json:"operation_type,omitempty"`
-	AssignTo      string   `json:"assign_to,omitempty"`
+	Type          string   `json:"type"`                     // Type of statement (block, variable_declaration, operation, function_call)
+	Variables     []string `json:"variables,omitempty"`      // List of declared variables
+	Uses          []string `json:"uses,omitempty"`           // List of variable uses
+	Block         Block    `json:"block,omitempty"`          // Nested block
+	Calls         []string `json:"calls,omitempty"`          // List of function calls
+	Parameters    []string `json:"arguments,omitempty"`      // List of function call parameters
+	OperationType string   `json:"operation_type,omitempty"` // Type of operation (e.g., addition, multiplication)
+	AssignTo      string   `json:"assign_to,omitempty"`      // Variable to assign the result of an operation
 }
 
 func main() {
