44c401d367a51388e02003c6ee41c63ae6e13aef
AhmedSoror <ahmedsoror438@gmail.com>
2023-05-18 14:24:18 +0200

    use common function to validate and different test cases

--- validatingProgram_test.go
+++ validatingProgram_test.go
@@ -5,135 +5,69 @@ import (
 	"testing"
 )
 
+// validateProgramTestCase a helper function to validate a test case
+func validateProgramTestCase(t *testing.T, filepath string, expectedResult bool) {
+	// Define the input program
+	program := ReadTestCaseFromJSON(filepath)
+
+	// Call the function
+	result := ValidateProgramRec(program, false)
+
+	// Compare the result with the expected output
+	if !reflect.DeepEqual(result, expectedResult) {
+		t.Errorf("Unexpected result. Got %v, want %v", result, expectedResult)
+	}
+}
+
 // --------------------------
 // Test valid programs
 // --------------------------
 func TestValidateProgramRec_OperationsOnly(t *testing.T) {
-	// Define the input program
-	program := ReadTestCaseFromJSON("./data/valid/operations.json")
-
-	// Define the expected output
 	expectedResult := true
-
-	// Call the function
-	result := ValidateProgramRec(program, false)
-
-	// Compare the result with the expected output
-	if !reflect.DeepEqual(result, expectedResult) {
-		t.Errorf("Unexpected result. Got %v, want %v", result, expectedResult)
-	}
+	filepath := "./data/valid/operations.json"
+	validateProgramTestCase(t, filepath, expectedResult)
 }
 func TestValidateProgramRec_FunctionCall(t *testing.T) {
-	// Define the input program
-	program := ReadTestCaseFromJSON("./data/valid/function_call.json")
-
-	// Define the expected output
 	expectedResult := true
-
-	// Call the function
-	result := ValidateProgramRec(program, false)
-
-	// Compare the result with the expected output
-	if !reflect.DeepEqual(result, expectedResult) {
-		t.Errorf("Unexpected result. Got %v, want %v", result, expectedResult)
-	}
+	filepath := "./data/valid/function_call.json"
+	validateProgramTestCase(t, filepath, expectedResult)
 }
 
 func TestValidateProgramRec_FunctionCallAsOperand(t *testing.T) {
-	// Define the input program
-	program := ReadTestCaseFromJSON("./data/valid/function_call_operand.json")
-
-	// Define the expected output
 	expectedResult := true
-
-	// Call the function
-	result := ValidateProgramRec(program, false)
-
-	// Compare the result with the expected output
-	if !reflect.DeepEqual(result, expectedResult) {
-		t.Errorf("Unexpected result. Got %v, want %v", result, expectedResult)
-	}
+	filepath := "./data/valid/function_call_operand.json"
+	validateProgramTestCase(t, filepath, expectedResult)
 }
 
 func TestValidateProgramRec_OperationAsOperand(t *testing.T) {
-	// Define the input program
-	program := ReadTestCaseFromJSON("./data/valid/operation_operand.json")
-
-	// Define the expected output
 	expectedResult := true
-
-	// Call the function
-	result := ValidateProgramRec(program, false)
-
-	// Compare the result with the expected output
-	if !reflect.DeepEqual(result, expectedResult) {
-		t.Errorf("Unexpected result. Got %v, want %v", result, expectedResult)
-	}
+	filepath := "./data/valid/operation_operand.json"
+	validateProgramTestCase(t, filepath, expectedResult)
 }
 
 // --------------------------
 // Test invalid programs
 // --------------------------
 func TestValidateProgramRec_UndeclaredFunction(t *testing.T) {
-	// Define the input program
-	program := ReadTestCaseFromJSON("./data/invalid/undeclared_func.json")
-
-	// Define the expected output
 	expectedResult := false
-
-	// Call the function
-	result := ValidateProgramRec(program, false)
-
-	// Compare the result with the expected output
-	if !reflect.DeepEqual(result, expectedResult) {
-		t.Errorf("Unexpected result. Got %v, want %v", result, expectedResult)
-	}
+	filepath := "./data/invalid/undeclared_func.json"
+	validateProgramTestCase(t, filepath, expectedResult)
 }
 
 func TestValidateProgramRec_UndeclaredVariable(t *testing.T) {
-	// Define the input program
-	program := ReadTestCaseFromJSON("./data/invalid/undeclared_var.json")
-
-	// Define the expected output
 	expectedResult := false
-
-	// Call the function
-	result := ValidateProgramRec(program, false)
-
-	// Compare the result with the expected output
-	if !reflect.DeepEqual(result, expectedResult) {
-		t.Errorf("Unexpected result. Got %v, want %v", result, expectedResult)
-	}
+	filepath := "./data/invalid/undeclared_var.json"
+	validateProgramTestCase(t, filepath, expectedResult)
 }
 
 func TestValidateProgramRec_UnassignedVariableInOperation(t *testing.T) {
-	// Define the input program
-	program := ReadTestCaseFromJSON("./data/invalid/operation_with_unassigned_variable.json")
-
-	// Define the expected output
 	expectedResult := false
-
-	// Call the function
-	result := ValidateProgramRec(program, false)
-
-	// Compare the result with the expected output
-	if !reflect.DeepEqual(result, expectedResult) {
-		t.Errorf("Unexpected result. Got %v, want %v", result, expectedResult)
-	}
+	filepath := "./data/invalid/operation_with_unassigned_variable.json"
+	validateProgramTestCase(t, filepath, expectedResult)
 }
 
 func TestValidateProgramRec_UnassignedVariableInFunctionCall(t *testing.T) {
-	// Define the input program
-	program := ReadTestCaseFromJSON("./data/invalid/function_call_with_unassigned_variable.json")
-
-	// Define the expected output
 	expectedResult := false
-
-	// Call the function
-	result := ValidateProgramRec(program, false)
-
-	// Compare the result with the expected output
-	if !reflect.DeepEqual(result, expectedResult) {
-		t.Errorf("Unexpected result. Got %v, want %v", result, expectedResult)
-	}
+	filepath := "./data/invalid/function_call_with_unassigned_variable.json"
+	validateProgramTestCase(t, filepath, expectedResult)
 }
